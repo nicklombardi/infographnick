afb791c98e4dd3355cc15d3047442b0e957bfd2f
nicklombardi <nicklombardi@gmail.com>
2013-09-22 09:16:01 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Infographnick::Application.config.secret_key_base = '8a9608104e90c51e468ee288ded3a93c29e44f5e51dbb0f209dc9d10e8cf5daf9a6f371476eeb52212cebe62e649458d01cb3349a5df40224500cb4b20569ba9'
